2b049feea0a880dfdf80003e108254dbc7b5e8be
Colin Stern <cstern19@gmail.com>
2017-09-18 12:37:40 -0400

    Updated tsa-bags

--- tsa-bags/data/draft-sets/p.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('test-set-unflattened.csv', 'r') as csvfile:
-	spamreader = csv.reader(csvfile)
-	data = list(spamreader)
-	for row in data:
-		print(row)
--- tsa-bags/data/draft-sets/pp.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('claims-data-2015-as-of-feb-9-2016-ADDEDLATLONG.csv', 'r') as csvfile:
-	spamreader = csv.reader(csvfile)
-	data = list(spamreader)
-	for row in data:
-		print(row)
